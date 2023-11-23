d002f5f9b58e9def9151d650f317d0c72f4e9ba4
AlissCava <cavalleraalissa@gmail.com>
2023-11-23 16:52:34 +0100

    problem,i bptone

--- script.js
+++ script.js
@@ -7,16 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextBtn = document.getElementById('nextBtn');
 
     nextBtn.addEventListener('click', function () {
-
         // Rimuoviamo la classe active all'elemento corrente
         items[activeItem].classList.remove('active');
-
         // Incrementiamo l'indice dell'elemento da visualizzare
         activeItem++;
         
+        // Verifica se siamo oltre la lunghezza dell'array, e in tal caso, torna all'inizio
+        if (activeItem >= items.length) {
+            activeItem = 0;
+        }
+
         // Aggiungiamo la classe active all'elemento successivo
         items[activeItem].classList.add('active');
 
+        // Mostrare il pulsante "Next" in caso di rimozione
+        nextBtn.classList.remove('hidden');
+        
         // Nascondiamo il pulsante "Next" se siamo all'ultimo elemento
         if (activeItem === items.length - 1) {
             nextBtn.classList.add('hidden');
